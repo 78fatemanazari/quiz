e6d18b783375e31646c5bad722031c457229c655
fatemanazari <fatemanazari78@gmail.com>
2024-02-06 21:02:38 +0330

    feat: Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'quiz#index'
+  get 'show_result', to: 'quiz#show_result'
+  resources :questions, only: [:index, :show] do
+    post 'submit_answer', on: :member
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
