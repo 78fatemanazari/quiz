3d51c37b4c4322c8778889b3cf754604f8c80c59
fatemanazari <fatemanazari78@gmail.com>
2024-02-06 21:08:13 +0330

    feat: fix rubocop offences

--- test/controllers/quiz_controller_test.rb
+++ test/controllers/quiz_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class QuizControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get quiz_index_url
     assert_response :success
   end
